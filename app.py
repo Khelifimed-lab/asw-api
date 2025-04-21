c9bb9b105199e40fa2155b2fbe0c5584bac6ab88
Khelifimed-lab <khelifimohammed04@gmail.com>
2025-04-21 08:55:31 +0100

    first commit

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 
 @app.route('/sketch', methods=['POST'])
 def sketch():
-    file = request.files['image']
-    img_array = np.frombuffer(file.read(), np.uint8)
+    # استلام الصورة كـ raw data
+    img_array = np.frombuffer(request.data, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    
+    if img is None:
+        return "Invalid image", 400
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     inv = 255 - gray
     blur = cv2.GaussianBlur(inv, (21, 21), 0)
