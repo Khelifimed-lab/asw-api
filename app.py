4517e1b1b57ca5f2b561136e4c796cb7e0323e80
Khelifimed-lab <khelifimohammed04@gmail.com>
2025-07-05 00:19:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,18 @@ import numpy as np
 import io
 
 app = Flask(__name__)
+
 @app.route('/sketch', methods=['POST'])
 def sketch():
     img_array = np.frombuffer(request.data, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-
+    
     if img is None:
         return "Invalid image", 400
 
-    # تحويل إلى رمادي
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # تحديد البكسلات السوداء (أقل من أو يساوي 30)
-    mask_black = gray <= 30
-
-    # إنشاء صورة بيضاء بالكامل
-    result = np.full_like(gray, 255)
-
-    # إرجاع البكسلات السوداء كما هي
-    result[mask_black] = gray[mask_black]
-
-    _, buf = cv2.imencode('.png', result)
+    inv = 255 - gray
+    blur = cv2.GaussianBlur(inv, (21, 21), 0)
+    sketch = cv2.divide(gray, 255 - blur, scale=256)
+    _, buf = cv2.imencode('.png', sketch)
     return send_file(io.BytesIO(buf), mimetype='image/png')
